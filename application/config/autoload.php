1c513809060f16e7e456ba785d6047a90f2a2562
Tasfir Hossain Suman <tasfir_suman@yahoo.com>
2015-08-26 16:27:33 +0600

    Updating All Cart option cart, update and delete

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -40,7 +40,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$autoload['packages'] = array();
+$autoload['packages'] = array(APPPATH.'third_party', '/usr/local/shared');
 
 
 /*
@@ -60,7 +60,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('database', 'email', 'session', 'table', 'cart');
 
 
 /*
@@ -88,7 +88,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('url', 'file', 'form');
 
 
 /*
@@ -104,7 +104,7 @@ $autoload['helper'] = array();
 |
 */
 
-$autoload['config'] = array();
+$autoload['config'] = array('config');
 
 
 /*
---
+++ application/controllers/Checkout.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Checkout
+ *
+ * @author tasfir
+ */
+class Checkout extends CI_Controller{
+    public function index(){
+        echo "hello World";
+    }
+}
